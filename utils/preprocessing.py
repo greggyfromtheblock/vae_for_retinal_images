f67acbb915260569d4f458ed17eb4d06fa687f79
Ruyoga <greg.pascalis@gmail.com>
2020-03-20 08:24:34 +0100

    resolving conflict

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -14,20 +14,11 @@ parser.add_argument('dir', type=str, default=None, metavar='image_dir',
                     help="""The path to the directory which contains
                     the imgages""")
 parser.add_argument('outdir', type=str, default=None, metavar='output_dir',
-                    help="""The path of the new directory where 
+                    help="""The path of the new directory where
                     they will be saved""")
 parser.add_argument('--resize', type=int, nargs=2, default=[0,0], metavar='resize shape',
                     help="""if resize is desired
                     use this option and give it 2 ints""")
-# parser.add_argument('--flip', action='store_true',
-#                     help="""if this flag is on
-#                     the script creates an addtional flipped version
-#                     of the images""")
-# parser.add_argument('--rotate', type=float, default=0, metavar='rotate',
-#                     help="""if this value is given
-#                     the script creates an addtional rotated version
-#                     of the images. The rotation is random between 0
-#                     and the provided argument""")
 args = parser.parse_args()
 
 
@@ -40,8 +31,6 @@ if args.outdir[-1] != '/':
 
 prefix = "preptest"
 outdir = "preptest/"
-db = 'odir/ODIR-5K_Training_Annotations(Updated)_V2.xlsx'
-imdir = 'odir/ODIR-5K_Training_Dataset/'
 
 outdir = args.outdir
 imdir = args.dir
@@ -68,10 +57,3 @@ for f in os.listdir(imdir):
     image = trim_image_rgb(image, args.resize)
     image = image.astype(np.uint8)
     io.imsave(outdir + f, image)
-    # if args.flip:
-    #     image_flipped = np.fliplr(image)
-    #     io.imsave(outdir + fname + "_flipped.jpg", image_flipped)
-    # if args.rotate > 0:
-    #     rot = args.rotate*random.random()
-    #     image_rotated = transform.rotate(image, rot)
-    #     io.imsave(outdir + fname + "_rotated.jpg", image_rotated)
