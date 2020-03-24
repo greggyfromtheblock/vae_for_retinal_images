7943664bf0dd784e550cca111c75cb706d7f2607
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-03-24 11:04:49 +0100

    Update decode_diagnostics_keywords.py

--- utils/decode_diagnostics_keywords.py
+++ utils/decode_diagnostics_keywords.py
@@ -33,39 +33,6 @@ import numpy as np
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(
-    description="""Reads the odir
-annotation file and asigns diagnostic codes for each side according
-to the diagnostic keywords. So in addition to field 'N', 'D' etc.
-The script adds fields 'LN','LD','RN','RD' etc.
-In additional there is a field 'L-ant', 'R-ant' where a non-zero
-indicates there is the special 'anterio segment image' keyword,
-and 'L-no', 'R-no' which indicates the keyword 'no fundus image'.
-"""
-)
-parser.add_argument(
-    "input",
-    type=str,
-    default=None,
-    metavar="input_file",
-    help="""The path to excel file of the annotations for
-                    the odir database training set."""
-)
-parser.add_argument(
-    "--out",
-    type=str,
-    default=sys.stdout,
-    metavar="--output_file",
-    help="""The path where to save the new
-             csv file. If no argument is given, the result is
-             printed to stdout and can be piped in the command line."""
-)
-parser.add_argument(
-    "--info",
-    action="store_true",
-    help="""Print information about how to use this script and exit"""
-)
-args = parser.parse_args()
 
 # Command: python3 Preprocessing/decode_diagnostics_keywords.py /home/henrik/PycharmProjects/Project\ A\ -\ VAE\
 # Retina/odir/ODIR-5K_Training_Annotations\(Updated\)_V2.xlsx --out /home/henrik/PycharmProjects/Project\ A\ -\ VAE\
@@ -92,142 +59,125 @@ O: other diagnosys except 'anterior segment image' and 'no fonndus image'
 special keywords: 'anterior segment image',  'no fonndus image'
 """
 
-if args.info:
-    print(info_text)
-    exit()
 
+def decode_d_k(path, output_file = "odir/odir_train_lr_annotations.csv"):
+    xsl_file = path + "odir/ODIR-5K_Training_Annotations(Updated)_V2.xlsx"
+    df = pd.read_excel(xsl_file)
 
-xslfile = args.input
-output_file = args.out
-df = pd.read_excel(xslfile)
+    # get all the unique diagnostics as a list
+    l = df["Left-Diagnostic Keywords"].tolist()
+    l = np.unique(l).tolist()
+    l = ",".join(l)
+    l = l.split(",")
+    l = np.unique(l).tolist()
+    s = ",".join(l)
+    s.replace(",", "")
+    np.unique(l)
+    s.split(",")
+    x = l[-1]
+    c = x[12]  # some weird char that looks like ', '
+    s = s.replace(c, ",")
+    l = s.split(",")
+    l = np.unique(l).tolist()  # now l realy contains the unique
 
+    # add separate left and right diagnotics columns instead of the
+    # joined one:
+    df["LN"] = np.zeros_like(df["N"])
+    df["LD"] = np.zeros_like(df["D"])
+    df["LG"] = np.zeros_like(df["G"])
+    df["LC"] = np.zeros_like(df["C"])
+    df["LA"] = np.zeros_like(df["A"])
+    df["LH"] = np.zeros_like(df["H"])
+    df["LM"] = np.zeros_like(df["M"])
+    df["LO"] = np.zeros_like(df["O"])
+    df["RN"] = np.zeros_like(df["N"])
+    df["RD"] = np.zeros_like(df["D"])
+    df["RG"] = np.zeros_like(df["G"])
+    df["RC"] = np.zeros_like(df["C"])
+    df["RA"] = np.zeros_like(df["A"])
+    df["RH"] = np.zeros_like(df["H"])
+    df["RM"] = np.zeros_like(df["M"])
+    df["RO"] = np.zeros_like(df["O"])
+    df["L-ant"] = np.zeros_like(df["O"])
+    df["L-no"] = np.zeros_like(df["O"])
+    df["R-ant"] = np.zeros_like(df["O"])
+    df["R-no"] = np.zeros_like(df["O"])
 
-# get all the unique diagnostics as a list
-l = df["Left-Diagnostic Keywords"].tolist()
-l = np.unique(l).tolist()
-l = ",".join(l)
-l = l.split(",")
-l = np.unique(l).tolist()
-s = ",".join(l)
-s.replace(",", "")
-np.unique(l)
-s.split(",")
-x = l[-1]
-c = x[12]  # some weird char that looks like ', '
-s = s.replace(c, ",")
-l = s.split(",")
-l = np.unique(l).tolist()  # now l realy contains the unique
-# diagnostics
-# l
+    ### Find All Diagnostic Keywords and Encode Them with:
+    feature = {
+        "N": "normal fundus",
+        "D": "proliferative retinopathy",
+        "G": "glaucoma",
+        "C": "catarct",
+        "A": "age related macular degeneration",
+        "H": "hypertensive retinopathy",
+        "M": "myopia",
+        "ant": "anterior segment",
+        "no": "no fundus image",
+    }
 
-# add separate left and right diagnotics columns instead of the
-# joined one:
-df["LN"] = np.zeros_like(df["N"])
-df["LD"] = np.zeros_like(df["D"])
-df["LG"] = np.zeros_like(df["G"])
-df["LC"] = np.zeros_like(df["C"])
-df["LA"] = np.zeros_like(df["A"])
-df["LH"] = np.zeros_like(df["H"])
-df["LM"] = np.zeros_like(df["M"])
-df["LO"] = np.zeros_like(df["O"])
-df["RN"] = np.zeros_like(df["N"])
-df["RD"] = np.zeros_like(df["D"])
-df["RG"] = np.zeros_like(df["G"])
-df["RC"] = np.zeros_like(df["C"])
-df["RA"] = np.zeros_like(df["A"])
-df["RH"] = np.zeros_like(df["H"])
-df["RM"] = np.zeros_like(df["M"])
-df["RO"] = np.zeros_like(df["O"])
-df["L-ant"] = np.zeros_like(df["O"])
-df["L-no"] = np.zeros_like(df["O"])
-df["R-ant"] = np.zeros_like(df["O"])
-df["R-no"] = np.zeros_like(df["O"])
+    # a function to search pattern in text
+    f = lambda pattern: lambda text: (pattern in text)
 
-### Find All Diagnostic Keywords and Encode Them with:
-feature = {
-    "N": "normal fundus",
-    "D": "proliferative retinopathy",
-    "G": "glaucoma",
-    "C": "catarct",
-    "A": "age related macular degeneration",
-    "H": "hypertensive retinopathy",
-    "M": "myopia",
-    "ant": "anterior segment",
-    "no": "no fundus image",
-}
+    np.vectorize(f("normal"))(df["Left-Diagnostic Keywords"])
 
-n = len(df)
-
-# feature['a'] in s
-
-# a function to search pattern in text
-f = lambda pattern: lambda text: (pattern in text)
-
-np.vectorize(f("normal"))(df["Left-Diagnostic Keywords"])
-
-# find features (except 'O') in Left, then Right Eye:
-for key, val in feature.items():
-    testl = np.vectorize(f(val))(df["Left-Diagnostic Keywords"])
-    testr = np.vectorize(f(val))(df["Right-Diagnostic Keywords"])
-    if key == "no":
-        #df["L-no"][testl] = 1  # special case 'no fundus'
-        df.loc[testl, "L-no"] = 1  # special case 'no fundus'
-        #df["R-no"][testr] = 1  # special case 'no fundus'
-        df.loc[testr, "R-no"] = 1  # special case 'no fundus'
-    elif key == "ant":
-        #df["L-ant"][testl] = 1  # special case 'ant'
-        df.loc[testl, "L-ant"] = 1  # special case 'ant'
-        #df["R-ant"][testr] = 1  # special case 'no fundus'
-        df.loc[testr, "R-ant"] = 1  # special case 'no fundus'
-    else:
-        #df["L" + key][testl] = 1
-        df.loc[testl, "L" + key] = 1
-        #df["R" + key][testr] = 1
-        df.loc[testr, "R" + key] = 1
-
-# remove feature keywors off the list of diagnostics
-# so only 'O' Diagnostics remain:
-olist = l.copy()
-for w in l:
+    # find features (except 'O') in Left, then Right Eye:
     for key, val in feature.items():
-        if val in w:
-            olist.remove(w)
+        testl = np.vectorize(f(val))(df["Left-Diagnostic Keywords"])
+        testr = np.vectorize(f(val))(df["Right-Diagnostic Keywords"])
+        if key == "no":
+            df.loc[testl, "L-no"] = 1  # special case 'no fundus'
+            df.loc[testr, "R-no"] = 1  # special case 'no fundus'
+        elif key == "ant":
+            df.loc[testl, "L-ant"] = 1  # special case 'ant'
+            df.loc[testr, "R-ant"] = 1  # special case 'no fundus'
+        else:
+            df.loc[testl, "L" + key] = 1
+            df.loc[testr, "R" + key] = 1
 
-olist.remove("lens dust")
-olist.remove("optic disk photographically invisible")
-olist.remove("low image quality")
-olist.remove("image offset")
+    # remove feature keywors off the list of diagnostics
+    # so only 'O' Diagnostics remain:
+    olist = l.copy()
+    for w in l:
+        for key, val in feature.items():
+            if val in w:
+                olist.remove(w)
 
-# Now find the 'O' (=all other) diagnostics:
-for val in olist:
-    testl = np.vectorize(f(val))(df["Left-Diagnostic Keywords"])
-    testr = np.vectorize(f(val))(df["Right-Diagnostic Keywords"])
-    df.loc[testl, "LO"]= 1
-    df.loc[testr, "RO"] = 1
+    olist.remove("lens dust")
+    olist.remove("optic disk photographically invisible")
+    olist.remove("low image quality")
+    olist.remove("image offset")
 
+    # Now find the 'O' (=all other) diagnostics:
+    for val in olist:
+        testl = np.vectorize(f(val))(df["Left-Diagnostic Keywords"])
+        testr = np.vectorize(f(val))(df["Right-Diagnostic Keywords"])
+        df.loc[testl, "LO"]= 1
+        df.loc[testr, "RO"] = 1
 
-df.to_csv(output_file, sep="\t", index=False, header=True)
+    # Making Left and Right each apear in separate row
+    cols = df.columns.tolist()
+    newcols = ['ID', 'Side', 'Patient Age', 'Patient Sex',
+               'Fundus Image', 'Diagnostic Keywords',
+               'N', 'D', 'G', 'C', 'A', 'H', 'M',
+               'O', 'anterior', 'no fundus']
 
-#xslfile = 'odir/ODIR-5K_Training_Annotations(Updated)_V2.xlsx'
-#df = pd.read_excel(xslfile)
+    left_df = pd.DataFrame(columns=['ID'])
+    left_df['ID'] = df['ID']
+    left_df['Side'] = "L"
+    left_df[newcols[2:5]] = df[cols[1:4]]
+    left_df['Diagnostic Keywords'] = df['Left-Diagnostic Keywords']
+    left_df[newcols[6:-2]] = df[cols[15:23]]
+    left_df[newcols[-2:]] = df[['L-ant', 'L-no']]
 
+    right_df = pd.DataFrame(columns=['ID'])
+    right_df['ID'] = df['ID']
+    right_df['Side'] = "R"
+    right_df[newcols[2:5]] = df[[cols[i] for i in [1, 2, 4]]]
+    right_df['Diagnostic Keywords'] = df['Right-Diagnostic Keywords']
+    right_df[newcols[6:-2]] = df[cols[23:-4]]
+    right_df[newcols[-2:]] = df[['R-ant', 'R-no']]
 
-## Example how the file was decoded
-# xslfile = 'odir/ODIR-5K_Training_Annotations(Updated)_V2.xlsx'
-# df.columns
-# df['Left-Diagnostic Keywords']
-# for i in range(100):
-#    print(df.loc[i])
-# df[df.D == 1]['Left-Diagnostic Keywords']
-# df[df.D == 1]['Right-Diagnostic Keywords']
-# df[df.G == 1]['Left-Diagnostic Keywords']
-# df[df.G == 1]['Right-Diagnostic Keywords']
-# df[df.C == 1]['Left-Diagnostic Keywords']
-# df[df.C == 1]['Right-Diagnostic Keywords']
-# df[df.A == 1]['Left-Diagnostic Keywords']
-# df[df.A == 1]['Right-Diagnostic Keywords']
-# df[df.H == 1]['Left-Diagnostic Keywords']
-# df[df.H == 1]['Right-Diagnostic Keywords']
-# df[df.M == 1]['Left-Diagnostic Keywords']
-# df[df.M == 1]['Right-Diagnostic Keywords']
-
+    new_df = pd.concat([left_df, right_df], axis=0)
+    new_df = new_df.sort_values(by=['ID', 'Side'])
+    new_df.to_csv(path+output_file, sep='\t', index=False, header=True)
