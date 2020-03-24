553e4e5d18b1980e6d51b5c5a82706b0c1a5f18e
Yiftach Kolb <zelhar@gmail.com>
2020-03-24 21:32:41 +0100

    preprocessing that works

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -76,41 +76,41 @@ if __name__ == "__main__":
 
     os.makedirs(ddir + outdir, exist_ok=True)
 
-#    print("Start cropping...")
-#    for f in tqdm(os.listdir(ddir + odir)):
-#        # Crop image
-#        trim_image_rgb(f, ddir + odir, ddir + outdir)
-#    print("Finished cropping...")
-#
-#    print("Start finding optimal image size and extend db...")
-#    opt_w, opt_h = find_optimal_image_size_and_extend_db(
-#            db = rootdir, 
-#            imdir = ddir + outdir, out="extended.tsv")
-#            imdir = ddir + outdir, out="odir/extended.tsv")
-#    print("Finished finding optimal image size and extend db...")
+    print("Start cropping...")
+    for f in tqdm(os.listdir(ddir + odir)):
+        # Crop image
+        trim_image_rgb(f, ddir + odir, ddir + outdir)
+    print("Finished cropping...")
 
-#    print("Start resizing and data augmentation...")
-#    for f in tqdm(os.listdir(ddir + outdir)):
-#        fname = f.replace(".jpg", "")
-#        image = io.imread(ddir + outdir + f)
-#
-#        # Resize image
-#        image = resize(image, output_shape=(opt_w, opt_h))
-#
-#        # save image under processed data
-#        io.imsave(ddir + outdir + f, img_as_ubyte(image))
-#
-#        # flip image
-#        image_flipped = np.fliplr(image)
-#        io.imsave(ddir + outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
-#
-#        # rotate image and save it
-#        rotate(image, ddir + outdir, fname)
-#        rotate(image_flipped, ddir + outdir, fname + "_flipped")
+    print("Start finding optimal image size and extend db...")
+    opt_w, opt_h = find_optimal_image_size_and_extend_db(
+            db = rootdir, 
+            imdir = ddir + outdir, out="extended.tsv")
+#            imdir = ddir + outdir, out="odir/extended.tsv")
+    print("Finished finding optimal image size and extend db...")
+
+    print("Start resizing and data augmentation...")
+    for f in tqdm(os.listdir(ddir + outdir)):
+        fname = f.replace(".jpg", "")
+        image = io.imread(ddir + outdir + f)
+
+        # Resize image
+        image = resize(image, output_shape=(opt_w, opt_h))
+
+        # save image under processed data
+        io.imsave(ddir + outdir + f, img_as_ubyte(image))
+
+        # flip image
+        image_flipped = np.fliplr(image)
+        io.imsave(ddir + outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
+
+        # rotate image and save it
+        rotate(image, ddir + outdir, fname)
+        rotate(image_flipped, ddir + outdir, fname + "_flipped")
 
     print("Finished resizing and data augmentation...")
 
     print("Decode diagnostics keywords...")
-    #decode_d_k(ddir )
+    #decode_d_k(ddir)
     decode_d_k(path=rootdir, output_file = "extended.csv" )
     print("Finished decoding diagnostics keywords...")
