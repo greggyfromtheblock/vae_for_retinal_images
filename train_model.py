2efe385b3ee979a955c5ac01a4df3aae70555906
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-03-26 18:34:56 +0100

    update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,32 +1,8 @@
 """
 Trigger training here
 """
-<<<<<<< HEAD
-
-from utils.utils import setup
-
-
-def prepare_datasets(logger, path_to_splits):
-    datasets = {'train': '')
-    return datasets
-
-
-if name == '__main__':
-    FLAGS, logger = setup(running_script="train_ECG_vae.py",
-                          config='config.json')
-
-    # input
-    split_data_path = FLAGS.input.strip().split(',')
-
-    datasets, eids = prepare_datasets(logger, split_data_path)
-
-    trained = train(logger, FLAGS, datasets['train'])
-
-    logger.info('Done.')
-=======
 import argparse
 import os
-import sys
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader, TensorDataset
@@ -37,42 +13,19 @@ import torch
 from utils.training import Encoder, Decoder, OdirVAETraining, VAEDataset
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            description="""Training VAE""")
-    parser.add_argument('imfolder', type=str, default=None,
-        metavar='image_dir',
-                    help="""The path to the directory which contains
-                    the imgage folder. The images themselves must be
-                    in one or more subdirectories of the imfolder""")
-    args = parser.parse_args()
-
-    def add_slash(path):
-        if path[-1] != '/':
-            return path + "/"
-        else:
-            return(path)
-    imfolder = add_slash(args.imfolder)
-
+if __name__ == '__main__':
     print("Load Data as Tensors...")
-#    img_dataset = datasets.ImageFolder(
-#        "./data/processed/", transform=transforms.ToTensor()
-#    )
-    img_dataset = datasets.ImageFolder(
-        imfolder, transform=transforms.ToTensor()
-    )
+    img_dataset = datasets.ImageFolder('./data/processed/', transform=transforms.ToTensor())
     data = VAEDataset(img_dataset)
 
     encoder, decoder = Encoder(), Decoder()
     training = OdirVAETraining(
-        encoder,
-        decoder,
-        data,
+        encoder, decoder, data,
         network_name="odir-vae",
-        device = "cuda" if torch.cuda.is_available() else "cpu",
+        device="cpu",
         batch_size=50,
         max_epochs=1000,
-        verbose=True,
+        verbose=True
     )
 
     training.train()
@@ -95,4 +48,3 @@ trained = train(logger, FLAGS, datasets['train'])
 
 logger.info('Done.')
 """
->>>>>>> e3c358b253f5d73c7291b1bda835560c5b8d279b
