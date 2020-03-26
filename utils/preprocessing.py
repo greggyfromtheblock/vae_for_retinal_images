8a49dfa0c3459d46b3a499a98bc5476ba7c82a4e
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-03-26 10:39:28 +0100

    merge updated preprocessing + training

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,62 +1,68 @@
-from __future__ import division, print_function
-
-import argparse
+from decode_diagnostics_keywords import decode_d_k
+from tqdm import tqdm
+from preprocessing_methods import trim_image_rgb, rotate, find_optimal_image_size_and_extend_db
 import os
-import random
-import warnings
-
+from skimage import io, img_as_ubyte
+from skimage.transform import resize
 import numpy as np
-from skimage import io
 
+
+# Ignore warnings
+import warnings
 warnings.filterwarnings("ignore")
 
-parser = argparse.ArgumentParser(description='Database Image Size Scanner')
-parser.add_argument('dir', type=str, default=None, metavar='image_dir',
-                    help="""The path to the directory which contains
-                    the imgages""")
-parser.add_argument('outdir', type=str, default=None, metavar='output_dir',
-                    help="""The path of the new directory where 
-                    they will be saved""")
-parser.add_argument('--resize', type=int, nargs=2, default=[0,0], metavar='resize shape',
-                    help="""if resize is desired
-                    use this option and give it 2 ints""")
-args = parser.parse_args()
 
+if __name__ == '__main__':
+    """
+    Preprocessing Steps:
+    Trim the black margins out of the image.
+    Find the 'optimal' image size, means: 
+        calculate the ratio of width and heigth of the cropped images,
+        resize images to minimal image size, if it is close to the avg. ratio,
+        hereby is purposed to avoid transforming the circle shape of retinas to ellipses by resizing the images
 
-# Correcting the possibly missing slash.
-# It's lame but works for this little purpose
-if args.dir[-1] != '/':
-    args.dir += '/'
-if args.outdir[-1] != '/':
-    args.outdir += '/'
+    Subsequently, the augmentation step follows:
+    Flip images, Rotate those images whose retinas are ca complete circles 
+    """
 
-prefix = "preptest"
-outdir = "preptest/"
-db = 'odir/ODIR-5K_Training_Annotations(Updated)_V2.xlsx'
-imdir = 'odir/ODIR-5K_Training_Dataset/'
+    dir = "/home/henrik/PycharmProjects/vae_for_retinal_images/data/"
+    odir = 'odir/ODIR-5K_Training_Dataset/'
+    outdir = 'processed/train/'
 
-outdir = args.outdir
-imdir = args.dir
-os.makedirs(outdir, exist_ok=True)
+    os.makedirs(dir+outdir, exist_ok=True)
+   
+     print("Start cropping...")
+    for f in tqdm(os.listdir(dir+odir)):
+        # Crop image
+        trim_image_rgb(f, dir+odir, dir+outdir)
+    print("Finished cropping...")
 
+    print("Start finding optimal image size and extend db...")
+    opt_w, opt_h = find_optimal_image_size_and_extend_db(dir)
+    print("Finished finding optimal image size and extend db...")
 
-def trim_image_rgb(img, r=0):
-    """Trims the black margins out of the image
-    The originaland returned images are rgb"""
-    ts = (img != 0).sum(axis=1) != 0
-    ts = ts.sum(axis=1) != 0
-    img = img[ts]
-    ts = (img != 0).sum(axis=0) != 0
-    ts = ts.sum(axis=1) != 0
-    img = img[:,ts,:]
-    if r != [0,0] and r != (0,0) and r !=0:
-        img = resize(img, r, anti_aliasing=True)
-    return img
+    print("Start resizing and data augmentation...")
+    for f in tqdm(os.listdir(dir+outdir)):
+        fname = f.replace(".jpg", "")
+        image = io.imread(dir+outdir + f)
 
-# Generate the standardized image set
-for f in os.listdir(imdir):
-    fname = f.replace(".jpg", "")
-    image = io.imread(imdir + f)
-    image = trim_image_rgb(image, args.resize)
-    image = image.astype(np.uint8)
-    io.imsave(outdir + f + "_cropped", image)
+        # Resize image
+        image = resize(image, output_shape=(opt_w, opt_h))
+
+        # save image under processed data
+        io.imsave(dir+outdir + f, img_as_ubyte(image))
+
+        # flip image
+        image_flipped = np.fliplr(image)
+        io.imsave(dir+outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
+
+        # rotate image and save it
+        rotate(image, dir+outdir, fname)
+        rotate(image_flipped, dir+outdir, fname + "_flipped")
+
+    print("Finished resizing and data augmentation...")
+
+    print("Decode diagnostics keywords...")
+    decode_d_k(dir)
+    print("Finished decoding diagnostics keywords...")
+
--- utils/training.py
+++ utils/training.py
@@ -1,3 +1,162 @@
 """
 Add the Training (TorchSupport-Training API) and loss functions here.
 """
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, TensorDataset
+from torchsupport.training.vae import VAETraining
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class VAEDataset(Dataset):
+    def __init__(self, data):
+        super(VAEDataset, self).__init__()
+        self.data = data
+
+    def __getitem__(self, index):
+        data, label = self.data[index]
+        return data,
+
+    def __len__(self):
+        return len(self.data)
+        pass
+
+
+def normalize(image):
+    return (image - image.min()) / (image.max() - image.min())
+
+
+class Encoder(nn.Module):
+    def __init__(self, z=32):
+        # Incoming image has shape e.g. 192x188x3
+        super(Encoder, self).__init__()
+
+        def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0, padding_max_pooling=0):
+            return [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                    nn.ReLU(),
+                    nn.BatchNorm2d(out_channels),
+                    nn.MaxPool2d(kernel_size=2, stride=2, padding=padding_max_pooling)]
+
+        self.conv_layers = nn.Sequential(
+            # Formula of new "Image" Size: (origanal_size - kernel_size + 2 * amount_of_padding)//stride + 1
+            *conv_block(3, 8, kernel_size=3, stride=1, padding=1),  # (192-3+2*1)//1 + 1 = 192  > Max-Pooling: 190/2=96
+            # -> (188-3+2*1)//1 + 1 = 188  --> Max-Pooling: 188/2 = 94
+            *conv_block(8, 16, kernel_size=3, padding=1),   # New "Image" Size:  48x44
+            *conv_block(16, 24, padding=1),     # New "Image" Size:  24x22
+            *conv_block(24, 36, padding=1, padding_max_pooling=1),   # New "Image" Size:  12x12
+            *conv_block(36, 54, padding=1),   # New "Image" Size:  6x6
+            *conv_block(54, 64, padding=1),   # New "Image" Size:  3*3
+        )
+
+        def linear_block(in_feat, out_feat, normalize=True, dropout=None):
+            layers = [nn.Linear(in_feat, out_feat)]
+            normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
+            dropout and layers.append(nn.Dropout(dropout))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.linear_layers = nn.Sequential(
+            # output_channels: 64; 3 x 3 from image dimension; 64*3*3 = 576
+            *linear_block(64 * 3 * 3, 320, normalize=False, dropout=0.5),
+            *linear_block(320, 256, dropout=0.5),
+            *linear_block(256, 128),
+            *linear_block(128, 64),
+            nn.Linear(64, z),
+            nn.ReLU()
+        )
+
+        self.mean = nn.Linear(z, z)
+        self.logvar = nn.Linear(z, z)
+
+    def forward(self, inputs):
+        features = self.conv_layers(inputs)
+        # print(features.shape)
+        features = features.view(-1, self.num_flat_features(features))
+        # print(features.shape)
+        features = self.linear_layers(features)
+
+        mean = self.mean(features)
+        logvar = self.logvar(features)
+        # print(features.shape)
+        return features, mean, logvar
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+class Decoder(nn.Module):
+    def __init__(self, z=32):
+        super(Decoder, self).__init__()
+
+        def linear_block(in_feat, out_feat, normalize=True, dropout=None):
+            layers = [nn.Linear(in_feat, out_feat)]
+            normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
+            dropout and layers.append(nn.Dropout(dropout))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.linear_blocks = nn.Sequential(
+            *linear_block(z, 64, normalize=False),
+            *linear_block(64, 256),
+            *linear_block(256, 320, dropout=0.5),
+            *linear_block(320, 576, dropout=0.5),   # 44*6*6 = 1584
+            nn.ReLU()
+        )
+
+        def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0):
+            return [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                    nn.ReLU(),
+                    nn.BatchNorm2d(out_channels),
+                    nn.Upsample(mode='bilinear', scale_factor=2)]
+
+        self.conv_layers = nn.Sequential(
+            *conv_block(64, 54, padding=1),
+            *conv_block(54, 36, padding=1),
+            *conv_block(36, 24, padding=1),
+            *conv_block(24, 8,  padding=1),
+            *conv_block(8, 3, kernel_size=5, padding=2),
+            nn.UpsamplingNearest2d(size=(192, 188)),  # The wished size
+            nn.Conv2d(in_channels=3, out_channels=3, kernel_size=1),
+        )
+
+    def forward(self, latent_vector):
+        dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 64, 3, 3))
+        # print(dec.shape)
+        reconstructions = self.conv_layers(dec)
+        print(reconstructions.shape)
+        return reconstructions
+
+
+class OdirVAETraining(VAETraining):
+    def run_networks(self, data, *args):
+        mean, logvar, reconstructions, data = super().run_networks(data, *args)
+        if self.step_id % 10 == 0:
+            self.writer.add_images("target", data[0:50:10], self.step_id)
+            self.writer.add_images("reconstruction", reconstructions[0:50:10], self.step_id)
+        return mean, logvar, reconstructions, data
+
+
+if __name__ == "__main__":
+    # Test Encoder
+    fake_imgs = torch.randn((10, 3, 192, 188))
+    # print(fake_imgs.shape)
+    encoder = Encoder()
+    # encoder.forward(fake_imgs)
+
+    # Test Decoder
+    fake_latent_vector = torch.randn((10, 32))
+    # print(233, fake_latent_vector.shape)
+    decoder = Decoder()
+    # decoder.forward(fake_latent_vector)
+
+
+
+
+
