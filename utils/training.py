dde2c8843adcef5a9f6fe2db6cae2b791260caa6
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-03-26 18:33:15 +0100

    update utils/training.py

--- utils/training.py
+++ utils/training.py
@@ -1,8 +1,5 @@
 """
 Add the Training (TorchSupport-Training API) and loss functions here.
-<<<<<<< HEAD
-"""
-=======
 """
 import torch
 import torch.nn as nn
@@ -11,7 +8,6 @@ from torchsupport.training.vae import VAETraining
 
 # Ignore warnings
 import warnings
-
 warnings.filterwarnings("ignore")
 
 
@@ -22,7 +18,7 @@ class VAEDataset(Dataset):
 
     def __getitem__(self, index):
         data, label = self.data[index]
-        return (data,)
+        return data,
 
     def __len__(self):
         return len(self.data)
@@ -38,47 +34,26 @@ class Encoder(nn.Module):
         # Incoming image has shape e.g. 192x188x3
         super(Encoder, self).__init__()
 
-        def conv_block(
-            in_channels,
-            out_channels,
-            kernel_size=3,
-            stride=1,
-            padding=0,
-            padding_max_pooling=0,
-        ):
-            return [
-                nn.Conv2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size=kernel_size,
-                    padding=padding,
-                    stride=stride,
-                ),
-                nn.ReLU(),
-                nn.BatchNorm2d(out_channels),
-                nn.MaxPool2d(kernel_size=2, stride=2, padding=padding_max_pooling),
-            ]
+        def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0, padding_max_pooling=0):
+            return [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                    nn.ReLU(),
+                    nn.BatchNorm2d(out_channels),
+                    nn.MaxPool2d(kernel_size=2, stride=2, padding=padding_max_pooling)]
 
         self.conv_layers = nn.Sequential(
             # Formula of new "Image" Size: (origanal_size - kernel_size + 2 * amount_of_padding)//stride + 1
-            *conv_block(
-                3, 8, kernel_size=3, stride=1, padding=1
-            ),  # (192-3+2*1)//1 + 1 = 192  > Max-Pooling: 190/2=96
+            *conv_block(3, 8, kernel_size=3, stride=1, padding=1),  # (192-3+2*1)//1 + 1 = 192  > Max-Pooling: 190/2=96
             # -> (188-3+2*1)//1 + 1 = 188  --> Max-Pooling: 188/2 = 94
-            *conv_block(8, 16, kernel_size=3, padding=1),  # New "Image" Size:  48x44
-            *conv_block(16, 24, padding=1),  # New "Image" Size:  24x22
-            *conv_block(
-                24, 36, padding=1, padding_max_pooling=1
-            ),  # New "Image" Size:  12x12
-            *conv_block(36, 54, padding=1),  # New "Image" Size:  6x6
-            *conv_block(54, 64, padding=1),  # New "Image" Size:  3*3
+            *conv_block(8, 16, kernel_size=3, padding=1),   # New "Image" Size:  48x44
+            *conv_block(16, 24, padding=1),     # New "Image" Size:  24x22
+            *conv_block(24, 36, padding=1, padding_max_pooling=1),   # New "Image" Size:  12x12
+            *conv_block(36, 54, padding=1),   # New "Image" Size:  6x6
+            *conv_block(54, 64, padding=1),   # New "Image" Size:  3*3
         )
 
         def linear_block(in_feat, out_feat, normalize=True, dropout=None):
             layers = [nn.Linear(in_feat, out_feat)]
-            normalize and layers.append(
-                nn.BatchNorm1d(out_feat)
-            )  # It's the same as: if normalize: append...
+            normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
             dropout and layers.append(nn.Dropout(dropout))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
@@ -90,7 +65,7 @@ class Encoder(nn.Module):
             *linear_block(256, 128),
             *linear_block(128, 64),
             nn.Linear(64, z),
-            nn.ReLU(),
+            nn.ReLU()
         )
 
         self.mean = nn.Linear(z, z)
@@ -122,9 +97,7 @@ class Decoder(nn.Module):
 
         def linear_block(in_feat, out_feat, normalize=True, dropout=None):
             layers = [nn.Linear(in_feat, out_feat)]
-            normalize and layers.append(
-                nn.BatchNorm1d(out_feat)
-            )  # It's the same as: if normalize: append...
+            normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
             dropout and layers.append(nn.Dropout(dropout))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
@@ -133,38 +106,28 @@ class Decoder(nn.Module):
             *linear_block(z, 64, normalize=False),
             *linear_block(64, 256),
             *linear_block(256, 320, dropout=0.5),
-            *linear_block(320, 576, dropout=0.5),  # 44*6*6 = 1584
-            nn.ReLU(),
+            *linear_block(320, 576, dropout=0.5),   # 44*6*6 = 1584
+            nn.ReLU()
         )
 
         def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0):
-            return [
-                nn.ConvTranspose2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size=kernel_size,
-                    padding=padding,
-                    stride=stride,
-                ),
-                nn.ReLU(),
-                nn.BatchNorm2d(out_channels),
-                nn.Upsample(mode="bilinear", scale_factor=2),
-            ]
+            return [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+                    nn.ReLU(),
+                    nn.BatchNorm2d(out_channels),
+                    nn.Upsample(mode='bilinear', scale_factor=2)]
 
         self.conv_layers = nn.Sequential(
             *conv_block(64, 54, padding=1),
             *conv_block(54, 36, padding=1),
             *conv_block(36, 24, padding=1),
-            *conv_block(24, 8, padding=1),
+            *conv_block(24, 8,  padding=1),
             *conv_block(8, 3, kernel_size=5, padding=2),
             nn.UpsamplingNearest2d(size=(192, 188)),  # The wished size
             nn.Conv2d(in_channels=3, out_channels=3, kernel_size=1),
         )
 
     def forward(self, latent_vector):
-        dec = torch.reshape(
-            self.linear_blocks(latent_vector), (latent_vector.shape[0], 64, 3, 3)
-        )
+        dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 64, 3, 3))
         # print(dec.shape)
         reconstructions = self.conv_layers(dec)
         print(reconstructions.shape)
@@ -174,18 +137,10 @@ class Decoder(nn.Module):
 class OdirVAETraining(VAETraining):
     def run_networks(self, data, *args):
         mean, logvar, reconstructions, data = super().run_networks(data, *args)
-        if self.step_id % 4 == 0:
-            self.writer.add_image("target", data[0], self.step_id)
-            self.writer.add_image("reconstruction", reconstructions[0], self.step_id)
+        if self.step_id % 10 == 0:
+            self.writer.add_images("target", data[0:50:10], self.step_id)
+            self.writer.add_images("reconstruction", reconstructions[0:50:10], self.step_id)
         return mean, logvar, reconstructions, data
-
-
-#        if self.step_id % 10 == 0:
-#            self.writer.add_images("target", data[0:50:10], self.step_id)
-#            self.writer.add_images(
-#                "reconstruction", reconstructions[0:50:10], self.step_id
-#            )
-#        return mean, logvar, reconstructions, data
 
 
 if __name__ == "__main__":
@@ -200,4 +155,8 @@ if __name__ == "__main__":
     # print(233, fake_latent_vector.shape)
     decoder = Decoder()
     # decoder.forward(fake_latent_vector)
->>>>>>> e3c358b253f5d73c7291b1bda835560c5b8d279b
+
+
+
+
+
