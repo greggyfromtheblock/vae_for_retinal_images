cb599720e48cf468130f83c303b4304f743276bc
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-03-29 15:20:52 +0200

    fixed preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import sys
 # Ignore warnings
 import warnings
 
@@ -15,12 +14,8 @@ from preprocessing_methods import (find_optimal_image_size_and_extend_db,
 
 warnings.filterwarnings("ignore")
 
-# Example command line run from within the utils folder:
-# python preprocessing.py ../../data/raw/ODIR_Training_Images/ process/images/ ../../data/train/ ../../data/raw/
-# the excel file should be in ../../data/raw and the raw images
-# should be inside a subfolder of it. 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Preprocessing Steps:
     Trim the black margins out of the image.
@@ -29,33 +24,24 @@ if __name__ == "__main__":
         resize images to minimal image size, if it is close to the avg. ratio,
         hereby is purposed to avoid transforming the circle shape of retinas to ellipses by resizing the images
     Subsequently, the augmentation step follows:
-    Flip images, Rotate those images whose retinas are ca complete circles 
-    Example command line, run from within 'utils':
-    python preprocessing.py ../../data/raw/ODIR_Training_Images/ process/images/ ../../data/train/ ../../data/raw/
+    Flip images, Rotate those images whose retinas are complete circles 
     """
-    parser = argparse.ArgumentParser(
-            description="""Preprocessing""")
-    parser.add_argument('rawdir', type=str, default=None,
-        metavar='raw_image_dir',
-                    help="""The path to the directory which contains
-                    the imgages""")
-    parser.add_argument('outdir_name', type=str, default=None,
-        metavar='output_dir_name',
-                    help="""The name of the new directory where 
-                    they will be saved""")
-    parser.add_argument('dir', type=str, default=None,
-        metavar='root_output_dir',
-                    help="""The path of the new directory where 
-                    they will be saved""")
-    parser.add_argument('root_dir', type=str, default=None,
-        metavar='root_input_dir',
-                    help="""The path of the directory where 
-                    the the odir folder is stored""")
-    args = parser.parse_args()
 
-    ddir = "/home/henrik/PycharmProjects/vae_for_retinal_images/data/"
-    odir = "odir/ODIR-5K_Training_Dataset/"
-    outdir = "processed/train/"
+    parser = argparse.ArgumentParser(
+        description="""Preprocessing""")
+    parser.add_argument('imdir', type=str, default=None,
+                        metavar='image_dir',
+                        help="""The path to the directory which contains
+                       the image folder. """)
+    parser.add_argument('outdir', type=str, default=None,
+                        metavar='image_dir',
+                        help="""The path to the directory which should contain
+                        processed augmented images.""")
+    parser.add_argument('xlsx_dir', type=str, default=None,
+                        metavar='image_dir',
+                        help="""The path to the directory which contains
+                       the Annotations of the Odir-Dataset. """)
+    args = parser.parse_args()
 
     def add_slash(path):
         if path[-1] != '/':
@@ -63,49 +49,50 @@ if __name__ == "__main__":
         else:
             return(path)
 
-    ddir = add_slash(args.dir)
-    odir = add_slash(args.rawdir)
-    outdir = add_slash(args.outdir_name)
-    rootdir = add_slash(args.root_dir)
+    dir = add_slash(args.imdir)
+    outdir = add_slash(args.outdir)
+    xlsx_dir = add_slash(args.xlsx_dir)
 
+    """
+    python3 utils/preprocessing.py
+    /home/henrik/PycharmProjects/vae_for_retinal_images/data/odir/ODIR-5K_Training_Dataset/
+    /home/henrik/PycharmProjects/vae_for_retinal_images/data/processed
+    /home/henrik/PycharmProjects/vae_for_retinal_images/data/odir
+    """
 
-    os.makedirs(ddir + outdir, exist_ok=True)
+    os.makedirs(outdir, exist_ok=True)
 
     print("Start cropping...")
-    for f in tqdm(os.listdir(ddir + odir)):
+    for i, f in tqdm(enumerate(os.listdir(dir))):
         # Crop image
-        trim_image_rgb(f, ddir + odir, ddir + outdir)
+        trim_image_rgb(f, dir, outdir)
     print("Finished cropping...")
 
     print("Start finding optimal image size and extend db...")
-    opt_w, opt_h = find_optimal_image_size_and_extend_db(
-            db = rootdir, 
-            imdir = ddir + outdir, out="extended.tsv")
-#            imdir = ddir + outdir, out="odir/extended.tsv")
+    opt_w, opt_h = find_optimal_image_size_and_extend_db(xlsx_dir, outdir)
     print("Finished finding optimal image size and extend db...")
 
     print("Start resizing and data augmentation...")
-    for f in tqdm(os.listdir(ddir + outdir)):
+    for f in tqdm(os.listdir(outdir)):
         fname = f.replace(".jpg", "")
-        image = io.imread(ddir + outdir + f)
+        image = io.imread(outdir + f)
 
         # Resize image
         image = resize(image, output_shape=(opt_w, opt_h))
 
         # save image under processed data
-        io.imsave(ddir + outdir + f, img_as_ubyte(image))
+        io.imsave(outdir + f, img_as_ubyte(image))
 
         # flip image
         image_flipped = np.fliplr(image)
-        io.imsave(ddir + outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
+        io.imsave(outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
 
         # rotate image and save it
-        rotate(image, ddir + outdir, fname)
-        rotate(image_flipped, ddir + outdir, fname + "_flipped")
+        rotate(image, outdir, fname)
+        rotate(image_flipped, outdir, fname + "_flipped")
 
     print("Finished resizing and data augmentation...")
 
     print("Decode diagnostics keywords...")
-    # decode_d_k(ddir)
-    decode_d_k(path=rootdir, output_file = "extended.csv" )
+    decode_d_k(xlsx_dir)
     print("Finished decoding diagnostics keywords...")
