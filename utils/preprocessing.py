c89c8638aab878f9f46a94543a73e82dd42a0341
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-03-31 20:14:50 +0200

    decouple the label preprocessing from the find_optimal_size_extend_db

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,22 +1,31 @@
-
-# TODO: maybe rename this file to preprocess_images.py and then the annotations file to preprocess_labels?
-
-import argparse
-import os
-import warnings
-
-import numpy as np
-from skimage import img_as_ubyte, io
-from skimage.transform import resize
-from tqdm import tqdm
-
 from decode_diagnostics_keywords import decode_d_k
-from preprocessing_methods import (find_optimal_image_size_and_extend_db,
-                                   rotate, trim_image_rgb)
+from tqdm import tqdm
+from preprocessing_methods import (
+    trim_image_rgb,
+    rotate,
+    find_optimal_image_size_and_extend_db,
+)
+import os
+from skimage import io, img_as_ubyte
+from skimage.transform import resize
+import numpy as np
+import sys
+import argparse
+
+
+
+
+# Ignore warnings
+import warnings
 
 warnings.filterwarnings("ignore")
 
-if __name__ == '__main__':
+# Example command line run from within the utils folder:
+# python preprocessing.py ../../data/raw/ODIR_Training_Images/ process/images/ ../../data/train/ ../../data/raw/
+# the excel file should be in ../../data/raw and the raw images
+# should be inside a subfolder of it. 
+
+if __name__ == "__main__":
     """
     Preprocessing Steps:
     Trim the black margins out of the image.
@@ -25,28 +34,33 @@ if __name__ == '__main__':
         resize images to minimal image size, if it is close to the avg. ratio,
         hereby is purposed to avoid transforming the circle shape of retinas to ellipses by resizing the images
     Subsequently, the augmentation step follows:
-    Flip images, Rotate those images whose retinas are complete circles 
+    Flip images, Rotate those images whose retinas are ca complete circles 
+    Example command line, run from within 'utils':
+    python preprocessing.py ../../data/raw/ODIR_Training_Images/ process/images/ ../../data/train/ ../../data/raw/
     """
-    # TODO: remove argparse for annotations directory
     parser = argparse.ArgumentParser(
-        description="""Preprocessing""")
-    parser.add_argument('imdir', type=str, default=None,
-                        metavar='image_dir',
-                        help="""The path to the directory which contains
-                       the image folder. """)
-    parser.add_argument('outdir', type=str, default=None,
-                        metavar='target_dir',
-                        help="""The path to the directory which should contain
-                        processed augmented images.""")
-    parser.add_argument('aug_per_image', type=int, default=0,
-                        metavar='n_augmentation',
-                        help="""Number of Augmented images per image""")
-    parser.add_argument('max_rotation_angle', type=int, default=0,
-                        metavar='maximum_rotation_angle',
-                        help="""Max rotation degree +- for the images, for example if you pass 10 to
-                        this argument then the function will pick {aug_per_image} random values from
-                        the range -10 to 10""")
-    args, unknown = parser.parse_known_args()
+            description="""Preprocessing""")
+    parser.add_argument('rawdir', type=str, default=None,
+        metavar='raw_image_dir',
+                    help="""The path to the directory which contains
+                    the imgages""")
+    parser.add_argument('outdir_name', type=str, default=None,
+        metavar='output_dir_name',
+                    help="""The name of the new directory where 
+                    they will be saved""")
+    parser.add_argument('dir', type=str, default=None,
+        metavar='root_output_dir',
+                    help="""The path of the new directory where 
+                    they will be saved""")
+    parser.add_argument('root_dir', type=str, default=None,
+        metavar='root_input_dir',
+                    help="""The path of the directory where 
+                    the the odir folder is stored""")
+    args = parser.parse_args()
+
+    ddir = "/home/henrik/PycharmProjects/vae_for_retinal_images/data/"
+    odir = "odir/ODIR-5K_Training_Dataset/"
+    outdir = "processed/train/"
 
     def add_slash(path):
         if path[-1] != '/':
@@ -54,31 +68,43 @@ if __name__ == '__main__':
         else:
             return(path)
 
-    dir = add_slash(args.imdir)
-    outdir = add_slash(args.outdir)
+    ddir = add_slash(args.dir)
+    odir = add_slash(args.rawdir)
+    outdir = add_slash(args.outdir_name)
+    rootdir = add_slash(args.root_dir)
 
-    # TODO: Uncouple resize function with preprocess annotations function on preprocessing_methods.py
+
+    os.makedirs(ddir + outdir, exist_ok=True)
+
+    print("Start cropping...")
+    for f in tqdm(os.listdir(ddir + odir)):
+        # Crop image
+        trim_image_rgb(f, ddir + odir, ddir + outdir)
+    print("Finished cropping...")
+
     print("Start finding optimal image size and extend db...")
-    opt_w, opt_h = find_optimal_image_size_and_extend_db(xlsx_dir, outdir)
+    opt_w, opt_h = find_optimal_image_size_and_extend_db(
+            imdir = ddir + outdir)
+#            imdir = ddir + outdir, out="odir/extended.tsv")
     print("Finished finding optimal image size and extend db...")
 
     print("Start resizing and data augmentation...")
-    for f in tqdm(os.listdir(outdir)):
+    for f in tqdm(os.listdir(ddir + outdir)):
         fname = f.replace(".jpg", "")
-        image = io.imread(outdir + f)
+        image = io.imread(ddir + outdir + f)
+
         # Resize image
         image = resize(image, output_shape=(opt_w, opt_h))
 
         # save image under processed data
-
-        io.imsave(outdir + f, img_as_ubyte(image))
+        io.imsave(ddir + outdir + f, img_as_ubyte(image))
 
         # flip image
         image_flipped = np.fliplr(image)
-        io.imsave(outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
+        io.imsave(ddir + outdir + fname + "_flipped.jpg", img_as_ubyte(image_flipped))
 
         # rotate image and save it
-        rotate(image, outdir, fname, args.aug_per_image, args.max_rotation_angle)
-        rotate(image_flipped, outdir, fname + "_flipped", args.aug_per_image, args.max_rotation_angle)
+        rotate(image, ddir + outdir, fname)
+        rotate(image_flipped, ddir + outdir, fname + "_flipped")
 
     print("Finished resizing and data augmentation...")
