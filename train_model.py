9e1a49d76feb0b970633a31fb541be856ed9771e
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-02 15:18:27 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,96 +3,80 @@ Trigger training here
 """
 import os
 import sys
-import argparse
-import numpy as np
-import torch
 from torchvision import datasets, transforms
-from torchvision.utils import save_image
-from torch.utils.data import DataLoader, TensorDataset
-from skimage import io
-from tqdm import tqdm
+import torch
 from utils.training import Encoder, Decoder, OdirVAETraining, VAEDataset
 from utils.utils import setup
+import time
+
 
 def normalize(image):
     return (image - image.min()) / (image.max() - image.min())
 
 
 if __name__ == "__main__":
+
     FLAGS, logger = setup(running_script="./utils/training.py", config="config.json")
     print("FLAGS= ", FLAGS)
 
-    def add_slash(path):
-        if path[-1] != '/':
-            return path + "/"
-        else:
-            return(path)
-
-#    imfolder = add_slash(args.imfolder)
     imfolder = os.path.abspath(FLAGS.input)
     device = FLAGS.device if torch.cuda.is_available() else "cpu"
 
-    if network_name in os.listdir(path_prefix):
-        input1 = input("Network already exists. Are you sure to continue? [y/yes]\n")
+    print("\ninput dir: ", imfolder,
+          "\ndevice: ", device)
+
+    if FLAGS.networkname in os.listdir(FLAGS.path_prefix):
+        input1 = input("\nNetwork already exists. Are you sure to proceed? ([y]/n) ")
         if not input1 in ['y', 'yes']:
             sys.exit()
 
-    print("input dir: ", imfolder,
-            "device: : ", device)
-
-    print("Load Data as Tensors...")
-    transform_data = transforms.Compose([transforms.ToTensor(), normalize])
-
-    img_dataset = datasets.ImageFolder(
-        imfolder, transform=transform_data
-    )
-
+    print("\nLoad Data as Tensors...")
+    img_dataset = datasets.ImageFolder(imfolder, transform=transforms.Compose([transforms.ToTensor(), normalize]))
     data = VAEDataset(img_dataset)
 
     encoder, decoder = Encoder(z=FLAGS.zdim), Decoder(z=FLAGS.zdim)
-
 
     training = OdirVAETraining(
         encoder,
         decoder,
         data,
-        network_name=FLAGS.networkname,
         path_prefix=FLAGS.path_prefix,
+        network_name=FLAGS.networkname,
         device=device,
+        optimizer_kwargs={"lr": FLAGS.learningrate},
         batch_size=FLAGS.batchsize,
         max_epochs=FLAGS.maxpochs,
         verbose=True,
     )
 
-    print("Start Training...")
-    trained = training.train()
-    print("Finished Training...")
+    print("\nSize of the dataset: {}\nShape of the single tensors: {}".format(len(data), data[0][0].shape))
+    print("\nTo check if values are between 0 and 1:\n{}".format(data[0][0][0][50][30:180:10]))
 
-    encoder = trained[0]
-    sample, _ = img_dataset[0]
-    features, mean, logvar = encoder(sample)
+    print("\nStart Training...")
+    time_start = time.time()
+    trained_encoder, _ = training.train()
+    print('\nTraining with %i epochs done! Time elapsed: %.2f minutes' % (FLAGS.maxpochs, (time.time() - time_start)/60))
+    # print(trained_encoder)
 
-    print(type(features))
-    if type(features) == torch.Tensor:
-        torch.save(features, f"{path_prefix}/{network_name}/features.pt")
-        torch.save(mean, f"{path_prefix}/{network_name}/mean.pt")
-        torch.save(logvar, f"{path_prefix}/{network_name}/logvar.pt")
-
-    if (type(features)) == np.ndarray:
-        np.save(f"{path_prefix}/{network_name}/features.npy", features)
-        np.save(f"{path_prefix}/{network_name}/mean.npy", mean)
-        np.save(f"{path_prefix}/{network_name}/logvar.npy", logvar)
+    # Save network
+    PATH = f'{FLAGS.path_prefix}/{FLAGS.networkname}/{FLAGS.networkname}.pth'
+    torch.save(trained_encoder.state_dict(), PATH)
 
 
-"""
+"""    
 def prepare_datasets(logger, path_to_splits):
 datasets = {'train': ''}
 return datasets
+
 FLAGS, logger = setup(running_script="train_ECG_vae.py",
                       config='config.json')
+
 # input
 split_data_path = FLAGS.input.strip().split(',')
+
 datasets, eids = prepare_datasets(logger, split_data_path)
+
 trained = train(logger, FLAGS, datasets['train'])
+
 logger.info('Done.')
 """
