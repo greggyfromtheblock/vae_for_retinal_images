c0ef5712cceb28472d74b4dc5dc21c71a6342d3d
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-07 12:27:01 +0200

    Update introspection.py

--- utils/introspection.py
+++ utils/introspection.py
@@ -5,16 +5,17 @@ from umap import UMAP
 from sklearn.manifold import TSNE
 import time
 import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib.patches as mpatches
 import pandas as pd
 from torchvision import datasets, transforms
 import numpy as np
 import torch
 import os
 from tqdm import tqdm
+
 from training import Encoder, VAEDataset
 from utils import setup
-import argparse
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -23,36 +24,27 @@ def normalize(image):
     return (image - image.min()) / (image.max() - image.min())
 
 
+def add_slash(path):
+    if path[-1] != '/':
+        return path + "/"
+    else:
+        return(path)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="""Introspection""")
-    parser.add_argument('imdir', type=str, default=None, metavar='image_dir',
-                        help="""The path to the directory which contains the preprocessed image folder.""")
-    parser.add_argument('network_dir', type=str, default=None, metavar='network_dir',
-                        help="""Directory which contains the trained encoder, the event files and torch files.""")
-    parser.add_argument('csv_file', type=str, default=None, metavar='csv_file',
-                        help="""The path to the preprocessed csv file.""")
-    parser.add_argument('network_dir', type=str, default=".",
-                        help="""Directory which contains the trained encoder, the event files and torch files.""")
-    parser.add_argument('network_name', type=str, default="vae",
-                        help="""Network Name.""")
-    args = parser.parse_args()
 
     FLAGS, logger = setup(running_script="./utils/introspection.py", config="config.json")
     print("FLAGS= ", FLAGS)
 
-    imfolder = os.path.abspath(args.imdir)
-    csv_file = os.path.abspath(args.csv_file)
-    network_dir = os.path.abspath(args.network_dir)
-    network_name = args.networkname
-
+    imfolder = add_slash(os.path.abspath(FLAGS.input))
+    csv_file = os.path.abspath(FLAGS.csv_input)
     latent_vector_size = FLAGS.zdim
-    path_prefix = FLAGS.path_prefix
 
-    sns.set_style('darkgrid')
-    sns.set_palette('muted')
-    sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
-    RS = 123
+    network_name = FLAGS.network_name
+    path_prefix = FLAGS.path_prefix
+    network_dir = f'{path_prefix}/{network_name}/'
+    network_vis_dir = f'{path_prefix}/{network_name}/visualizations/'
+
 
     print("\nLoad Data as Tensors...")
     transform_data = transforms.Compose([transforms.ToTensor(), normalize])
@@ -64,10 +56,8 @@ if __name__ == '__main__':
 
     print("\nBuild targets...")
     csv_df = pd.read_csv(csv_file, sep='\t')
-    # print(csv_df['Fundus Image'])
-    # print(list(csv_df.columns))
 
-    features = {
+    diagnoses = {
         "N": "normal fundus",
         "D": "proliferative retinopathy",
         "G": "glaucoma",
@@ -78,48 +68,55 @@ if __name__ == '__main__':
         # "ant": "anterior segment",
         # "no": "no fundus image",
     }
-    number_of_diagnoses = len(features)  # not sure if others should be an own category
-    targets = np.zeros((len(data), number_of_diagnoses))
+    number_of_diagnoses = len(diagnoses)  # not sure if others should be an own category
+    data_size = len(data)
+    targets = np.zeros((data_size, number_of_diagnoses),  dtype=np.int8)
 
     angles = [x for x in range(-FLAGS.max_degree, -9)]
     angles.extend([x for x in range(10, FLAGS.max_degree+1)])
     angles.extend([x for x in range(-9, 9)])
     print("\nPossible Angles: {}\n".format(angles))
 
-    for i, jpg in tqdm(enumerate(os.listdir(imfolder))):
+    for i, jpg in tqdm(enumerate(os.listdir(imfolder+"/Images"))):
         jpg = jpg.replace("_flipped", "")
 
         for angle in angles:
             jpg = jpg.replace("_rot_%i" % angle, "")
 
         row_number = csv_df.loc[csv_df['Fundus Image'] == jpg].index[0]
-        for j, feature in enumerate(features.keys()):
+        for j, feature in enumerate(diagnoses.keys()):
             targets[i][j] = csv_df.iloc[row_number].at[feature]
 
     print("Finished building targets...")
 
     # Load network
     trained_encoder = Encoder()
-    trained_encoder.load_state_dict(torch.load(network_dir+f"/{network_name}" + ".pth"))
+    trained_encoder.load_state_dict(torch.load(network_dir+f"{network_name}.pth"))
+
+    print("Generate samples..")
+    samples = torch.zeros((data_size, *data[0][0].shape))
+    encoded_samples = np.zeros((data_size, latent_vector_size))
+    for i in tqdm(range(0, data_size, data_size)):
+        samples[i] = data[i][0]
 
     print("\nStart encoding of each image...")
-    encoded_samples = np.zeros((len(data), latent_vector_size))
-    for i, sample in tqdm(enumerate(data[0])):
-        features, _, _ = trained_encoder(sample[0])
-        encoded_samples[i] = features.detach().numpy()
+    features, _, _ = trained_encoder(samples)
+    encoded_samples = features.detach().numpy()
     print("Finished encoding of each image...")
 
+    os.makedirs(network_dir+"/Visualizations", exist_ok=True)
     print("Start Visualization...")
-    for i, feature in tqdm(enumerate(features.keys())):
+    colormap = np.array(['darkorange', 'royalblue'])
+
+    for i, diagnosis in tqdm(enumerate(diagnoses.keys())):
 
         # tSNE Visualization of the encoded latent vector
         time_start = time.time()
-        tsne = TSNE(random_state=RS).fit_transform(encoded_samples)
-        print('t-SNE done! Time elapsed: {} seconds'.format(time.time() - time_start))
+        tsne = TSNE(random_state=123).fit_transform(encoded_samples)
 
         tsne_df = pd.DataFrame({'X': tsne[:, 0],
                                 'Y': tsne[:, 1],
-                                features[feature]: np.transpose(targets[i])})
+                                f'{diagnoses[diagnosis]}': targets[:, i]})
 
         # U-Map Visualization
         clusterable_embedding = UMAP(
@@ -128,61 +125,27 @@ if __name__ == '__main__':
             n_components=2,
             random_state=42,
         ).fit_transform(encoded_samples)
+
         umap_df = pd.DataFrame({'X': clusterable_embedding[:, 0],
                                 'Y': clusterable_embedding[:, 1],
-                                features[feature]: np.transpose(targets[i])})
+                                diagnoses[diagnosis]: targets[:, i]})
 
-        plt.subplot(2, 1, 1)
-        sns.scatterplot(x="X", y="Y",
-                        hue=feature,
-                        palette=['orange', 'blue'],
-                        legend='full',
-                        data=tsne_df);
+        orange_patch = mpatches.Patch(color=colormap[0], label=f'No {diagnoses[diagnosis]}')
+        blue_patch = mpatches.Patch(color=colormap[1], label=f'{diagnoses[diagnosis]}')
+        diagnosis_name = diagnoses[diagnosis]
 
-        plt.title("tSNE-Visualization")
+        plt.scatter(tsne[:, 0], tsne[:, 1], c=colormap[targets[:, i]], s=100)
+        plt.legend(handles=[orange_patch, blue_patch])
+        plt.title(f"tSNE-Visualization of diagnosis: {diagnosis_name}\n", fontsize=16, fontweight='bold')
 
-        plt.subplot(2, 1, 2)
-        sns.scatterplot(x="X", y="Y",
-                        hue=feature,
-                        palette=['purple', 'red', 'orange', 'brown', 'blue',
-                                 'dodgerblue', 'green', 'lightgreen', 'darkcyan', 'black'],
-                        legend='full',
-                        data=umap_df);
-
-        # plt.scatter(clusterable_embedding[:, 0], clusterable_embedding[:, 1], c=targets, s=0.1, cmap='Spectral');
-        plt.title("UMAP-Visualization")
-
-        fea = features[feature]
-        plt.savefig(f"{path_prefix}/{network_name}/visualization_for_feature_{fea}.png")
+        plt.savefig(f"{path_prefix}/{network_name}/Visualizations/tsne_visualization_of_diagnosis_{diagnosis_name}.png")
         plt.show()
         plt.close()
 
+        plt.scatter(clusterable_embedding[:, 0], clusterable_embedding[:, 1], c=colormap[targets[:, i]], s=100, label=colormap)
+        plt.legend(colormap)
+        plt.title(f"UMAP-Visualization of diagnosis: {diagnosis_name}\n", fontsize=16, fontweight='bold')
 
-    """
-     csv_df = pd.read_csv(path, sep='\t')
-    >>> a=list(csv_df.columns)
-    >>> a
-    ['ID', 'Side', 'Patient Age', 'Patient Sex', 'Fundus Image', 'Diagnostic Keywords', 'N',
-     'D', 'G', 'C', 'A', 'H', 'M', 'O', 'anterior', 'no fundus']
-    >>> d=csv_df.'ID'
-      File "<stdin>", line 1
-        d=csv_df.'ID'
-                    ^
-    SyntaxError: invalid syntax
-    >>> d=csv_df['ID']
-    >>> d=csv_df['Fundus Image']
-    >>> d
-    0           0_left.jpg
-    1          0_right.jpg
-    2           1_left.jpg
-    3          1_right.jpg
-    4           2_left.jpg
-                 ...      
-    6995    4689_right.jpg
-    6996     4690_left.jpg
-    6997    4690_right.jpg
-    6998     4784_left.jpg
-    6999    4784_right.jpg
-    Name: Fundus Image, Length: 7000, dtype: object
-    >>> 
-    """
+        plt.savefig(f"{path_prefix}/{network_name}/Visualizations/umap_visualization_of_diagnosis_{diagnosis_name}.png")
+        plt.show()
+        plt.close()
