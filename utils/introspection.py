d545a6e604ee11e5ad9f2ccf9335e17e4b8df2dd
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-07 18:43:56 +0200

    Update introspection.py

--- utils/introspection.py
+++ utils/introspection.py
@@ -72,17 +72,15 @@ if __name__ == '__main__':
 
     angles = [x for x in range(-FLAGS.max_degree, -9)]
     angles.extend([x for x in range(10, FLAGS.max_degree+1)])
-    angles.extend([x for x in range(-9, 9)])
+    angles.extend([x for x in range(-9, 10)])
     print("\nPossible Angles: {}\n".format(angles))
-    print(os.getcwd)
-    print(imfolder)
+ 
     for i, jpg in tqdm(enumerate(os.listdir(imfolder))):
         jpg = jpg.replace("_flipped", "")
 
         for angle in angles:
             jpg = jpg.replace("_rot_%i" % angle, "")
         
-        print(jpg)
         row_number = csv_df.loc[csv_df['Fundus Image'] == jpg].index[0]
         for j, feature in enumerate(diagnoses.keys()):
             targets[i][j] = csv_df.iloc[row_number].at[feature]
