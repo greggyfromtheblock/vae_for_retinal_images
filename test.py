601b2685e3ee84c1d7c47b8856c255e976077307
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-09 15:11:10 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+"""
+Add plotting and introspection functions here
+"""
+
+import pandas as pd
+
+import os
+from tqdm import tqdm
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+if __name__ == '__main__':
+    imfolder = "/data/analysis/ag-reils/ag-reils-shared-students/retina/data/raw/ODIR_Testing_Images"
+    csv_file = "/data/analysis/ag-reils/ag-reils-shared-students/{user}/vae_for_retinal_images/data/processed/annotations/ODIR_Annotations.csv"
+    csv_df = pd.read_csv(csv_file, sep='\t')
+
+    list_not_suitable_jpgs_for_introspection_because_of_absence_in_annotations = []
+    for jpg in tqdm(os.listdir(folder)):
+        if not csv_df.loc[csv_df['Fundus Image'] == jpg].index[0]:
+            list_not_suitable_jpgs_for_introspection_because_of_absence_in_annotations.append(jpg)
+
+    print(list_not_suitable_jpgs_for_introspection_because_of_absence_in_annotations)
+
