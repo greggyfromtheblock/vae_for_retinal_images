397b90fec76fbb071ad9826d8b12fb276cc1d41b
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-04-14 16:56:51 +0200

    updoot train model

--- train_model.py
+++ train_model.py
@@ -1,101 +1,88 @@
+  
 """
 Trigger training here
 """
-import argparse
 import os
 import sys
-
-import numpy as np
+import time
 
 import torch
 from torchvision import datasets, transforms
 from utils.training import Decoder, Encoder, OdirVAETraining, VAEDataset
+from utils.utils import setup
 
 
 def normalize(image):
     return (image - image.min()) / (image.max() - image.min())
 
 
+def add_slash(path):
+    if path[-1] != '/':
+        return path + "/"
+    else:
+        return(path)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            description="""Training VAE""")
-    parser.add_argument('imfolder', type=str, default=None,
-        metavar='image_dir',
-                    help="""The path to the directory which contains
-                    the imgge folder. The images themselves must be
-                    in one or more subdirectories of the imfolder""")
-    parser.add_argument('--path_prefix', type=str, default=".",
-        metavar='path_prefix',
-                    help="""The path to the directory which should contain the data for tensorboard.""")
-    parser.add_argument('network_name', type=str, default=None,
-        metavar='network_name',
-                    help="""The name of the network. Use different names for different models!""")
-    args = parser.parse_args()
 
-    def add_slash(path):
-        if path[-1] != '/':
-            return path + "/"
-        else:
-            return(path)
+    FLAGS, logger = setup(running_script="./utils/training.py", config="config.json")
 
-    imfolder = add_slash(args.imfolder)
-    network_name = args.network_name
-    path_prefix = args.path_prefix  # optional argument. If default: the path is the current one.
+    imfolder = add_slash(FLAGS.input)
+    network_name = FLAGS.network_name
+    path_prefix = FLAGS.path_prefix
 
-    if network_name in os.listdir(path_prefix):
-        input1 = input("Network already exists. Are you sure to continue? [y/yes]\n")
+    network_dir = f'{path_prefix}/{network_name}/'
+
+    device = FLAGS.device if torch.cuda.is_available() else "cpu"
+
+    print("\ninput dir: ", imfolder,
+          "\npath prefix: ", path_prefix,
+          "\nnetwork name: ", network_name,
+          "\nlearningrate: ", FLAGS.learningrate,
+          "\nbatch size: ", FLAGS.batchsize,
+          "\nmax epochs: ", FLAGS.maxepochs,
+          "\nz-dim: ", FLAGS.zdim,
+          "\nmax degree: ", FLAGS.maxdegree,
+          "\ndevice: ", device)
+
+    os.makedirs(network_dir, exist_ok=True)
+    if FLAGS.network_name in os.listdir(network_dir):
+        input1 = input("\nNetwork already exists. Are you sure to proceed? ([y]/n) ")
         if not input1 in ['y', 'yes']:
             sys.exit()
 
-    print("Load Data as Tensors...")
-    transform_data = transforms.Compose([transforms.ToTensor(), normalize])
+    print("\nLoad Data as Tensors...")
     img_dataset = datasets.ImageFolder(
-        imfolder, transform=transform_data
-    )
-
+        # Because dataloader asks for the parent directory
+        os.path.dirname(os.path.dirname(imfolder)),
+        transform=transforms.Compose([transforms.ToTensor(), normalize]))
     data = VAEDataset(img_dataset)
-    encoder, decoder = Encoder(), Decoder()
+
+    encoder, decoder = Encoder(z=FLAGS.zdim), Decoder(z=FLAGS.zdim)
+
     training = OdirVAETraining(
         encoder,
         decoder,
         data,
         path_prefix=path_prefix,
         network_name=network_name,
-        device="cuda:3" if torch.cuda.is_available() else "cpu",
-        batch_size=128,
-        max_epochs=100,
-        verbose=True
+        device=device,
+        optimizer_kwargs={"lr": FLAGS.learningrate},
+        batch_size=FLAGS.batchsize,
+        max_epochs=FLAGS.maxepochs,
+        verbose=True,
     )
 
-    print("Start Training...")
-    trained = training.train()
-    print("Finished Training...")
+    print("\nSize of the dataset: {}\nShape of the single tensors: {}".format(len(data), data[0][0].shape))
+    print("\nTo check if values are between 0 and 1:\n{}".format(data[0][0][0][50][30:180:10]))
 
-    encoder = trained[0]
-    sample, _ = img_dataset[0]
-    features, mean, logvar = encoder(sample)
+    print("\nStart Training...")
+    time_start = time.time()
+    trained_encoder, _ = training.train()
+    print('\nTraining with %i epochs done! Time elapsed: %.2f minutes' % (FLAGS.maxpochs, (time.time() - time_start)/60))
+    # print(trained_encoder)
 
-    print(type(features))
-    if type(features) == torch.Tensor:
-        torch.save(features, f"{path_prefix}/{network_name}/features.pt")
-        torch.save(mean, f"{path_prefix}/{network_name}/mean.pt")
-        torch.save(logvar, f"{path_prefix}/{network_name}/logvar.pt")
-
-    if (type(features)) == np.ndarray:
-        np.save(f"{path_prefix}/{network_name}/features.npy", features)
-        np.save(f"{path_prefix}/{network_name}/mean.npy", mean)
-        np.save(f"{path_prefix}/{network_name}/logvar.npy", logvar)
-
-
-"""
-def prepare_datasets(logger, path_to_splits):
-datasets = {'train': ''}
-return datasets
-FLAGS, logger = setup(running_script="train_ECG_vae.py",
-                      config='config.json')
-# input
-split_data_path = FLAGS.input.strip().split(',')
-datasets, eids = prepare_datasets(logger, split_data_path)
-trained = train(logger, FLAGS, datasets['train'])
-logger.info('Done.')
-"""
+    # Save network
+    # os.makedirs(network_dir)
+    PATH = network_dir+f'{network_name}.pth'
+    torch.save(trained_encoder.state_dict(), PATH)
