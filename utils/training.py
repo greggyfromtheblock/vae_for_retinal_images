811ccd888c4af9e7010c18e820dcec3637956094
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-16 18:07:55 +0200

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -43,13 +43,13 @@ class Encoder(nn.Module):
 
         self.conv_layers = nn.Sequential(
             # Formula of new "Image" Size: (origanal_size - kernel_size + 2 * amount_of_padding)//stride + 1
-            *conv_block(3, 32, kernel_size=5, stride=1, padding=2),  # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
+            *conv_block(3, 16, kernel_size=5, stride=1, padding=2),  # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
             # -> (188-5+2*2)//1 + 1 = 188  --> Max-Pooling: 188/2 = 94
-            *conv_block(32, 64, kernel_size=5, padding=2),   # New "Image" Size:  48x47
-            *conv_block(64, 128, padding=1),  # New "Image" Size:  24x23
-            *conv_block(128, 256, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
-            *conv_block(256, 512, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
-            *conv_block(512, 256, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
+            *conv_block(16, 32, kernel_size=5, padding=2),   # New "Image" Size:  48x47
+            *conv_block(32, 64, padding=1),  # New "Image" Size:  24x23
+            *conv_block(64, 96, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
+            *conv_block(96, 128, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
+            *conv_block(128, 256, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
         )
 
         def linear_block(in_feat, out_feat, normalize=True, dropout=None, negative_slope=1e-2):
@@ -60,15 +60,17 @@ class Encoder(nn.Module):
             return layers
 
         self.linear_layers = nn.Sequential(
-            *linear_block(64 * 5 * 4, 512, normalize=True, dropout=0.5),
-            # *linear_block(512, 256, dropout=0.3),
-            # *linear_block(256, 128),
-            *linear_block(512, 64),
+            *linear_block(256 * 2 * 2, 512, normalize=True, dropout=0.6),
+            *linear_block(512, 512, normalize=True, dropout=0.6),
+            *linear_block(512, 256, dropout=0.4),
+            *linear_block(256, 128),
+            *linear_block(128, 128),
+            *linear_block(128, 64),
             *linear_block(64, z, negative_slope=0.0)
         )
 
-        self.mean = nn.Linear(1024, z)
-        self.logvar = nn.Linear(1024, z)
+        self.mean = nn.Linear(z, z)
+        self.logvar = nn.Linear(z, z)
 
     def forward(self, inputs):
         features = self.conv_layers(inputs)
@@ -76,20 +78,11 @@ class Encoder(nn.Module):
         # features = features.view(-1, self.num_flat_features(features))
         features = features.view(-1, np.prod(features.shape[1:]))
         # print(features.shape)
-        # features = self.linear_layers(features)
+        features = self.linear_layers(features)
         # print(8,features.shape)
         mean = self.mean(features)
         logvar = self.logvar(features)
         return features, mean, logvar
-
-    @staticmethod
-    def num_flat_features(x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
 
 class Decoder(nn.Module):
     def __init__(self, z=32):
@@ -105,9 +98,11 @@ class Decoder(nn.Module):
         self.linear_blocks = nn.Sequential(
             *linear_block(z, 64, normalize=True),
             *linear_block(64, 128),
-            # *linear_block(128, 256, dropout=None),
-            # *linear_block(256, 512, dropout=0.3),
-            *linear_block(128, 1280, dropout=0.5),  # 5*4*64
+            *linear_block(128, 128),
+            *linear_block(128, 256, dropout=None),
+            *linear_block(256, 256, dropout=0.3),
+            *linear_block(256, 512, dropout=0.3),
+            *linear_block(512, 512 * 2 * 2, dropout=0.4),
             nn.ReLU()
         )
 
@@ -127,20 +122,18 @@ class Decoder(nn.Module):
             return layers
 
         self.conv_layers = nn.Sequential(
-            *conv_block(8, 64, padding=1, size=(5, 4)),
-            *conv_block(64, 128, padding=1, size=(11, 10)),
-            *conv_block(128, 256, padding=1, size=(24, 23), mode='nearest'),
-            *conv_block(256, 128, padding=1, size=(48, 47), mode='nearest'),
-            *conv_block(128, 64, padding=1, scale_factor=2),
-            *conv_block(64, 32, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
-            nn.Conv2d(in_channels=32, out_channels=3, kernel_size=1),
-            # nn.BatchNorm2d(3)
-            # nn.Sigmoid()
+            *conv_block(512, 256, padding=1, size=(5, 4)),
+            *conv_block(256, 128, padding=1, size=(11, 10)),
+            *conv_block(128, 96, padding=1, size=(24, 23), mode='nearest'),
+            *conv_block(96, 64, padding=1, size=(48, 47), mode='nearest'),
+            *conv_block(64, 32, padding=1, scale_factor=2),
+            *conv_block(32, 8, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
+            nn.Conv2d(in_channels=8, out_channels=3, kernel_size=1),
         )
 
-    def forward(self, latent_vectors):
-        # dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 64, 5, 4))
-        reconstructions = self.conv_layers(torch.reshape(latent_vectors, (latent_vectors.shape[0], 8, 2, 2)))
+    def forward(self, latent_vector):
+        dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 512, 2, 2))
+        reconstructions = self.conv_layers(dec)
         return reconstructions
 
 
@@ -171,9 +164,14 @@ class OdirVAETraining(VAETraining):
             self.epoch = self.epoch_id
             print("%i-Epoch" % (self.epoch_id+1))
 
-        if self.step_id % 5 == 0:
-            self.writer.add_image("target", data[0], self.step_id)
-            self.writer.add_image("reconstruction", F.sigmoid(reconstructions[0]), self.step_id)
+        imgs = torch.zeros_like(reconstructions[0:50:10])
+
+        for i in range(0, 50, 10):
+            imgs[i] = F.sigmoid(reconstructions[i])
+
+        if self.step_id % 20 == 0:
+            self.writer.add_images("target", data[0:50:10], self.step_id)
+            self.writer.add_images("reconstruction", imgs, self.step_id)
         return mean, logvar, reconstructions, data
 
 
