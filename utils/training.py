73af9e83e1efe99c8734e5b650106d7a92b0f9db
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-20 10:25:57 +0200

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -29,27 +29,26 @@ class VAEDataset(Dataset):
 
 class Encoder(nn.Module):
     def __init__(self, z=32):
-        # Incoming image has shape e.g. 192x188x3
+        # Incoming image has shape e.g. 384x376x3
         super(Encoder, self).__init__()
 
         def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0, padding_max_pooling=0):
-            return [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2, stride=stride),
-                    nn.ReLU(),
-                    nn.BatchNorm2d(out_channels),
-                    nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+            return [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
                     nn.ReLU(),
                     nn.BatchNorm2d(out_channels),
                     nn.MaxPool2d(kernel_size=2, stride=2, padding=padding_max_pooling)]
 
         self.conv_layers = nn.Sequential(
             # Formula of new "Image" Size: (origanal_size - kernel_size + 2 * amount_of_padding)//stride + 1
-            *conv_block(3, 16, kernel_size=5, stride=1, padding=2),  # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
+            *conv_block(3, 6, kernel_size=5, stride=1, padding=2), # (384-5+2*2)//1 + 1 = 384  > Max-Pooling: 384/2=192
+            # -> (376-5+2*2)//1 + 1 = 376  --> Max-Pooling: 376/2 = 188
+            *conv_block(6, 12, kernel_size=5, stride=1, padding=2), # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
             # -> (188-5+2*2)//1 + 1 = 188  --> Max-Pooling: 188/2 = 94
-            *conv_block(16, 32, kernel_size=5, padding=2),   # New "Image" Size:  48x47
-            *conv_block(32, 64, padding=1),  # New "Image" Size:  24x23
-            *conv_block(64, 96, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
-            *conv_block(96, 128, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
-            *conv_block(128, 256, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
+            *conv_block(12, 48, kernel_size=5, padding=2),   # New "Image" Size:  48x47
+            *conv_block(48, 96, padding=1),  # New "Image" Size:  24x23
+            *conv_block(96, 128, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
+            *conv_block(128, 256, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
+            *conv_block(256, 512, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
         )
 
         def linear_block(in_feat, out_feat, normalize=True, dropout=None, negative_slope=1e-2):
@@ -60,13 +59,13 @@ class Encoder(nn.Module):
             return layers
 
         self.linear_layers = nn.Sequential(
-            *linear_block(256 * 2 * 2, 512, normalize=True, dropout=0.6),
-            *linear_block(512, 512, normalize=True, dropout=0.6),
-            *linear_block(512, 256, dropout=0.4),
-            *linear_block(256, 128),
-            *linear_block(128, 128),
-            *linear_block(128, 64),
-            *linear_block(64, z, negative_slope=0.0)
+            *linear_block(512 * 2 * 2, 256, normalize=True, dropout=0),
+            *linear_block(256, 256, normalize=True, dropout=0),
+            *linear_block(256, 128, dropout=0),
+            *linear_block(128, 96),
+            *linear_block(96, 96),
+            *linear_block(96, 48),
+            *linear_block(48, z, negative_slope=0.0)
         )
 
         self.mean = nn.Linear(z, z)
@@ -75,7 +74,6 @@ class Encoder(nn.Module):
     def forward(self, inputs):
         features = self.conv_layers(inputs)
         # print(features.shape)
-        # features = features.view(-1, self.num_flat_features(features))
         features = features.view(-1, np.prod(features.shape[1:]))
         # print(features.shape)
         features = self.linear_layers(features)
@@ -96,23 +94,19 @@ class Decoder(nn.Module):
             return layers
 
         self.linear_blocks = nn.Sequential(
-            *linear_block(z, 64, normalize=True),
-            *linear_block(64, 128),
+            *linear_block(z, 48, normalize=True),
+            *linear_block(48, 96),
+            *linear_block(96, 128),
             *linear_block(128, 128),
             *linear_block(128, 256, dropout=None),
-            *linear_block(256, 256, dropout=0.3),
-            *linear_block(256, 512, dropout=0.3),
-            *linear_block(512, 512 * 2 * 2, dropout=0.4),
+            *linear_block(256, 512, dropout=0),
+            *linear_block(512, 512 * 2 * 2, dropout=0),
             nn.ReLU()
         )
 
         def conv_block(in_channels, out_channels, kernel_size=3, stride=1, padding=0, scale_factor=None, size=None, mode='bilinear'):
 
-            layers = [nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=kernel_size//2,
-                                stride=stride),
-                      nn.BatchNorm2d(in_channels),
-                      nn.ReLU(),
-                      nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding,
+            layers = [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding,
                                          stride=stride),
                       nn.BatchNorm2d(out_channels),
                       nn.ReLU()]
@@ -127,13 +121,15 @@ class Decoder(nn.Module):
             *conv_block(128, 96, padding=1, size=(24, 23), mode='nearest'),
             *conv_block(96, 64, padding=1, size=(48, 47), mode='nearest'),
             *conv_block(64, 32, padding=1, scale_factor=2),
-            *conv_block(32, 8, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
-            nn.Conv2d(in_channels=8, out_channels=3, kernel_size=1),
+            *conv_block(32, 12, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
+            *conv_block(12, 6, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
+            nn.Conv2d(in_channels=6, out_channels=3, kernel_size=1),
         )
 
     def forward(self, latent_vector):
         dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 512, 2, 2))
         reconstructions = self.conv_layers(dec)
+        print(reconstructions.shape)
         return reconstructions
 
 
@@ -176,7 +172,7 @@ class OdirVAETraining(VAETraining):
 
 
 if __name__ == '__main__':
-    fake_imgs = torch.randn((4, 3, 192, 188))
+    fake_imgs = torch.randn((4, 3, 384, 376))
     encoder = Encoder()
     encoder(fake_imgs)
     fake_latent_vectors = torch.randn((10, 32))
