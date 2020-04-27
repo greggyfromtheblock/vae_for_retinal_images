ea2d7582fc63cb7e556ef8277afdacc1ec7da30d
Yiftach Kolb <zelhar@gmail.com>
2020-04-27 16:44:14 +0200

    trying normalizing

--- train_model_pretrained.py
+++ train_model_pretrained.py
@@ -54,7 +54,10 @@ if __name__ == "__main__":
     img_dataset = datasets.ImageFolder(
         imfolder,
         transform=transforms.Compose(
-            [transforms.ToTensor(),]
+#            [transforms.ToTensor(),]
+            [transforms.ToTensor(),
+                transforms.Normalize(
+                    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
             #                transforms.Normalize((0.5,), (0.5,))]
         ),
     )
