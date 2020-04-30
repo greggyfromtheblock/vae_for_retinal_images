98f5f8303d08d8fe7b7553545ce8e0f8054bc46a
henrik997 <44259278+henrik997@users.noreply.github.com>
2020-04-30 12:30:48 +0200

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -46,30 +46,33 @@ class Encoder(nn.Module):
 
         self.conv_layers = nn.Sequential(
             # Formula of new "Image" Size: (origanal_size - kernel_size + 2 * amount_of_padding)//stride + 1
-            *conv_block(3, 32, kernel_size=5, stride=1, padding=2),  # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
+            *conv_block(3, 100, kernel_size=5, stride=1, padding=2),  # (192-5+2*2)//1 + 1 = 192  > Max-Pooling: 190/2=96
             # -> (188-5+2*2)//1 + 1 = 188  --> Max-Pooling: 188/2 = 94
-            *conv_block(64, 64, kernel_size=5, padding=2),   # New "Image" Size:  48x47
-            *conv_block(64, 96, padding=1),  # New "Image" Size:  24x23
-            *conv_block(96, 128, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
-            *conv_block(128, 256, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
-            *conv_block(256, 512, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
+            *conv_block(100, 150, kernel_size=5, padding=2),   # New "Image" Size:  48x47
+            *conv_block(150, 200, padding=1),  # New "Image" Size:  24x23
+            *conv_block(200, 300, padding=0, padding_max_pooling=1),  # New "Image" Size:  11x10
+            *conv_block(300, 400, padding=0, padding_max_pooling=0),  # New "Image" Size:  5x4
+            *conv_block(400, 500, padding=0, padding_max_pooling=1),  # New "Image" Size:  2x2
         )
 
-        def linear_block(in_feat, out_feat, normalize=True, dropout=None, negative_slope=1e-2):
+        def linear_block(in_feat, out_feat, normalize=True, dropout=None, negative_slope = None):
             layers = [nn.Linear(in_feat, out_feat)]
             normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
             dropout and layers.append(nn.Dropout(dropout))
-            layers.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=True))
+            if negative_slope:
+                layers.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=True))
+            else:
+                layers.append(nn.ReLU())
             return layers
 
         self.linear_layers = nn.Sequential(
-            *linear_block(512 * 2 * 2, 512, normalize=True, dropout=0.6),
+            *linear_block(500 * 2 * 2, 512, normalize=True, dropout=0.2),
             *linear_block(512, 512, normalize=True, dropout=0.5),
-            *linear_block(512, 256, dropout=0.4),
-            *linear_block(256, 128),
-            *linear_block(128, 128),
-            *linear_block(128, 64),
-            *linear_block(64, z, negative_slope=0.0)
+            *linear_block(512, 256, dropout=0.5),
+            *linear_block(256, 64, negative_slope=1e-2),
+            # *linear_block(128, 128, negative_slope=1e-2),
+            # *linear_block(128, 64, negative_slope=1e-2),
+            *linear_block(64, z)
         )
 
         self.mean = nn.Linear(z, z)
@@ -91,21 +94,23 @@ class Decoder(nn.Module):
     def __init__(self, z=32):
         super(Decoder, self).__init__()
 
-        def linear_block(in_feat, out_feat, normalize=True, dropout=None):
+        def linear_block(in_feat, out_feat, normalize=True, dropout=None, negative_slope = None):
             layers = [nn.Linear(in_feat, out_feat)]
             normalize and layers.append(nn.BatchNorm1d(out_feat))  # It's the same as: if normalize: append...
             dropout and layers.append(nn.Dropout(dropout))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            if negative_slope:
+                layers.append(nn.LeakyReLU(negative_slope=negative_slope, inplace=True))
+            else:
+                layers.append(nn.ReLU())
             return layers
 
         self.linear_blocks = nn.Sequential(
             *linear_block(z, 64, normalize=True),
-            *linear_block(64, 128),
-            *linear_block(128, 128),
-            *linear_block(128, 256, dropout=None),
-            *linear_block(256, 256, dropout=0.3),
-            *linear_block(256, 512, dropout=0.3),
-            *linear_block(512, 512 * 2 * 2, dropout=0.4),
+            *linear_block(64, 256),
+            # *linear_block(128, 256),
+            *linear_block(256, 512, negative_slope=1e-2),
+            *linear_block(512, 512, dropout=0.2),
+            *linear_block(512, 500 * 2 * 2, dropout=0.3),
             nn.ReLU()
         )
 
@@ -125,17 +130,17 @@ class Decoder(nn.Module):
             return layers
 
         self.conv_layers = nn.Sequential(
-            *conv_block(512, 256, padding=1, size=(5, 4)),
-            *conv_block(256, 128, padding=1, size=(11, 10)),
-            *conv_block(128, 96, padding=1, size=(24, 23), mode='nearest'),
-            *conv_block(96, 64, padding=1, size=(48, 47), mode='nearest'),
-            *conv_block(64, 32, padding=1, scale_factor=2),
-            *conv_block(32, 8, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
-            nn.Conv2d(in_channels=8, out_channels=3, kernel_size=1),
+            *conv_block(500, 400, padding=1, size=(5, 4)),
+            *conv_block(400, 300, padding=1, size=(11, 10)),
+            *conv_block(300, 250, padding=1, size=(24, 23), mode='nearest'),
+            *conv_block(250, 200, padding=1, size=(48, 47), mode='nearest'),
+            *conv_block(200, 150, padding=1, scale_factor=2),
+            *conv_block(150, 50, kernel_size=5, padding=2, scale_factor=2, mode='nearest'),
+            nn.Conv2d(in_channels=50, out_channels=3, kernel_size=1),
         )
 
     def forward(self, latent_vector):
-        dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 512, 2, 2))
+        dec = torch.reshape(self.linear_blocks(latent_vector), (latent_vector.shape[0], 500, 2, 2))
         reconstructions = self.conv_layers(dec)
         return reconstructions
 
@@ -231,23 +236,25 @@ class OdirFactorVAETraining(VAETraining):
 
     def run_networks(self, data, *args):
         mean, logvar, reconstructions, data = super().run_networks(data, *args)
+        sample = self.sample(mean, logvar)
 
         if self.epoch != self.epoch_id:
             self.epoch = self.epoch_id
             print("%i-Epoch" % (self.epoch_id + 1))
 
-        imgs = torch.zeros_like(reconstructions[0:50:10])
+        if data.size(0) > 49:
+            imgs = torch.zeros_like(reconstructions[0:50:10])
+            for i in range(0, 5):
+                imgs[i] = F.sigmoid(reconstructions[i * 10])
 
-        for i in range(0, 50, 10):
-            imgs[i] = F.sigmoid(reconstructions[i])
+            if self.step_id % 20 == 0:
+                self.writer.add_images("target", data[0:50:10], self.step_id)
+                self.writer.add_images("reconstruction", imgs, self.step_id)
 
-        if self.step_id % 20 == 0:
-            self.writer.add_images("target", data[0:50:10], self.step_id)
-            self.writer.add_images("reconstruction", imgs, self.step_id)
-        return (mean, logvar), reconstructions, data
+        return (mean, logvar), reconstructions, sample
 
     def step(self, data):
-        data = to_device(data, self.device)
+        data = to_device(data[0], self.device)
         sample_data, shuffle_data = data[:data.size(0) // 2], data[data.size(0) // 2:]
         # könnte man einbauen:
         """
