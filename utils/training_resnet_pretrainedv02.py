3ebf14af966dbe9d4996502018bd91915152c6d2
Yiftach Kolb <zelhar@gmail.com>
2020-05-09 20:27:05 +0200

    f

--- utils/training_resnet_pretrainedv02.py
+++ utils/training_resnet_pretrainedv02.py
@@ -366,8 +366,10 @@ class Encoder(nn.Module):
         model = models.resnet101(pretrained=pretrained).cuda() #output is [-1, 1000]
         #model = resnetCustom(3,z).cuda()
         if state_dict != None:
-            model.state_dict = torch.load(state_dict, map_location='cuda') 
-            print("model.state_dict: ", model.state_dict)
+            model.load_state_dict(state_dict, map_location='cuda')
+            #model.state_dict = torch.load(state_dict, map_location='cuda') 
+            model.train()
+            print("model.state_dict loaded ")
 
         model.fc = nn.Linear(model.fc.in_features, z, bias=True)
 
